75460988c745a2dd96021087440b2d0bff54eb6d
toons22 <anthony2.peron@orange.com>
2025-10-04 04:58:30 +0000

    quatrieme reformat du fichier de tests

--- file_test.py
+++ file_test.py
@@ -2,23 +2,23 @@ import pytest
 
 
 def test_calc_addition():
-  output = 2+4
-  assert output == 6
+    output = 2+4
+    assert output == 6
 
 
 def test_calc_substraction():
-  # Fonction test du résultat de 2-4
-  output = 2-4
-  assert output == -2
+    # Fonction test du résultat de 2-4
+    output = 2-4
+    assert output == -2
 
 
 def test_calc_multiply():
-  # Fonction test du résultat de 2*4
-  output = 2*4
-  assert output == 8
+    # Fonction test du résultat de 2*4
+    output = 2*4
+    assert output == 8
 
 
 def test_coucou():
-  # Fonction test si la résultat renvoie 'hello'
-  output = 'hello'
-  assert output == 'hello'
+    # Fonction test si la résultat renvoie 'hello'
+    output = 'hello'
+    assert output == 'hello'
